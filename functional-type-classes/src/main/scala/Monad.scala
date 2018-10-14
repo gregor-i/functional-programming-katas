28c43f2d6e835102fa41ac1db8bd069ce0d95fde
Gregor Ihmor <gregor_alias1@outlook.de>
2018-10-14 15:46:55 +0200

    removed solution

--- functional-type-classes/src/main/scala/Monad.scala
+++ functional-type-classes/src/main/scala/Monad.scala
@@ -1,5 +1,1 @@
-trait Monad[F[_]] extends Functor[F]{
-  def pure[A](a: A): F[A]
-  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
-  def map[A, B](data: F[A])(mapper: A => B): F[B] = flatMap(data)(mapper andThen pure)
-}
+//
